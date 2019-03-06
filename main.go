f46124fec009f782a6a04adf1bdbd62d3bfa1cd8
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-03-06 21:59:19 +0700

    [Added]: compute gradient image in matrix

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
+	// initial empty image matrix
 	imageResult := make([][]uint8, dy)
-	for index := 0; index < dy; index++ {
-		imageResult[index] = make([]uint8, dx)
+	for y := 0; y < dy; y++ {
+		imageResult[y] = make([]uint8, dx)
+	}
+	// compute image in matrix
+	for y := 0; y < dy; y++ {
+		for x := 0; x < dx; x++ {
+			imageResult[y][x] = uint8((x + y) / 2)
+		}
 	}
 	return imageResult
 }
